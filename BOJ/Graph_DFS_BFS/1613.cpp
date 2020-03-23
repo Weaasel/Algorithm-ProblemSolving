59b644a766067834a44194428d5d57b081de8be7
Weaasel <46833351+Weaasel@users.noreply.github.com>
2020-03-23 15:30:17 +0900

    역사

---
+++ BOJ/Graph_DFS_BFS/1613.cpp
@@ -0,0 +1,59 @@
+//역사
+//DAG, Topological sort
+
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#define pb push_back
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define MAX 
+#define DIV 1000000
+#define INF 2000000000
+
+using namespace std;
+
+int n, k, s, u, v;
+bool adj[401][401];
+
+void floyd() {
+	for (int k = 1; k <= n; k++) {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				if (adj[i][k] && adj[k][j]) {
+					adj[i][j] = true;
+				}
+			}
+		}
+	}
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> u >> v;
+		adj[u][v] = true;
+	}
+	floyd();
+	cin >> s;
+	for (int i = 0; i < s; i++) {
+		cin >> u >> v;
+		if (adj[u][v]) cout << "-1\n";
+		else if (adj[v][u]) cout << "1\n";
+		else cout << "0\n";
+	}
+	return 0;
+}
