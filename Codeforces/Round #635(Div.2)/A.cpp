9adddc69e04c1b276183a9eaffe69de6b3bb2a4c
Yunje Lee <yjyj102742@gmail.com>
2020-04-16 23:56:13 +0900

    Create A.cpp

---
+++ Codeforces/Round #635(Div.2)/A.cpp
@@ -0,0 +1,35 @@
+//A. Ichihime and Triangle
+
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#define pb push_back
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(v) (v).begin(),(v).end()
+#define MAX 4001
+#define DIV 100000LL
+#define INF 2000000000
+
+using namespace std;
+int a, b, c, d, T;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> T;
+	while (T--) {
+		cin >> a >> b >> c >> d;
+		cout << b << " " << c << " " << c << "\n";
+	}
+	return 0;
+}
