9c1aec82e53d46e1b2c799beb55c33512c6466b2
Yunje Lee <yjyj102742@gmail.com>
2020-10-18 00:16:20 +0900

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest 180/A.cpp
@@ -0,0 +1,15 @@
+//Box
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define pb push_back
+using namespace std;
+
+int n, a, b;
+
+int main() {
+	cin >> n >> a >> b;
+	cout << n - a + b;
+	return 0;
+}
