696681baa0d8cfa3b5c9f57db892b9c0c93a7ce0
Yunje Lee <yjyj102742@gmail.com>
2020-10-26 18:40:20 +0900

    Create Dijkstra.cpp

---
+++ BOJ/Shortest Path/Dijkstra.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#define pb push_back
+#define pii pair<int, int>
+#define INF 1e+9 + 7
+
+int n, m, s;
+int dist[20202];
+vector<pii> adj[20202];
+
+void dijkstra(int s) {
+	dist[s] = 0;
+	priority_queue<pii> pq;
+	pq.push({ 0,s });
+	while (!pq.empty()) {
+		pii p = pq.top(); pq.pop();
+		int cur = p.second, curd = -p.first;
+		if (dist[cur] < curd) continue;
+		for (pii np : adj[cur]) {
+			int nxt = np.first, d = np.second;
+			if (dist[nxt] < curd + d) continue;
+			dist[nxt] = curd + d;
+			pq.push({ -dist[nxt],nxt });
+		}
+	}
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	cin >> n >> m >> s;
+	fill(dist+1, dist + n + 1, INF);
+
+	for (int i = 0; i < m; i++) {
+		int u, v, c; cin >> u >> v >> c;
+		adj[u].pb({ v,c });
+	}
+	dijkstra(s);
+  return 0;
+}
