6624a53cf29bcce700941178b80cd11c7751a165
Yunje Lee <yjyj102742@gmail.com>
2020-11-02 17:32:40 +0900

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest 181/A.cpp
@@ -0,0 +1,14 @@
+//A - Heavy Rotation	
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	cin >> n;
+	if (n % 2 == 0) cout << "White";
+	else cout << "Black";
+	return 0;
+}
