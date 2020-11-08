a4dad8193d7fd1bb1d906822dd2bfb8ec2976394
Yunje Lee <yjyj102742@gmail.com>
2020-11-08 16:31:24 +0900

    Update 14595.cpp

--- BOJ/DP/14595.cpp
+++ BOJ/DP/14595.cpp
@@ -1,42 +1,18 @@
 //동방 프로젝트(Large)
 //Dynamic programming, prefix sum, sweeping
+/*
+ 결국 남아있는 방의 개수라는 것은 (남아있는 벽의 수 + 1) 과 같다.
+ 현재 1~n번 방 사이사이를 연결하는 n-1개의 벽이 있고, a~b번 방을 합친다는 말은 방a ~ 방b-1 을 없앤다는 말과 같다.
+ 따라서, (a,b)로 들어오는 쿼리에 대해 p[a]--, p[b]++을 해주고, 마지막에 부분합 스위핑을 진행하며 값이 0이상인, 즉 지워진 적이 없는 벽의 개수만 세어준다.
+*/
 
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <functional>
-#include <stack>
-#include <queue>
-#include <cstdlib>
-#include <cstring>
-#include <map>
-#include <set>
-#include <bitset>
-#include <deque>
-#include <complex>
-#include <limits.h>
-#include <list>
-#define all(v) (v).begin(),(v).end()
-#define sz(v) ((v).size())
-#define F(i,s,e) for(int i=s;i<=e;i++)
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vl = vector<ll>;
-using pdd = pair<double, double>;
-using pil = pair<int, ll>;
 
 int n, m;
 int p[1000010];
 
 int main() {
-	/*#ifndef ONLINE_JUDGE
-	freopen("output.txt", "w", stdout);
-	#endif*/
 	ios_base::sync_with_stdio(false);
 	cin.tie(0); cout.tie(0);
 	
