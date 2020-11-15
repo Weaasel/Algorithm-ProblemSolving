356b50010cf35c8e419ebc1684b8e92adb6387d8
Yunje Lee <yjyj102742@gmail.com>
2020-11-16 01:04:27 +0900

    Create A.cpp

---
+++ AtCoder/AtCoder Beginner Contest 183/A.cpp
@@ -0,0 +1,29 @@
+//ReLU
+/*
+  Beginner's Contest의 관례대로 출력문제가 나왔다.
+*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<ll, ll>
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define INF 1e15+7
+#define eps 1e-8
+#define DIV 998244353
+#define pi 3.14159265
+
+using namespace std;
+
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n;
+	if (n >= 0) cout << n;
+	else cout << 0;
+
+	return 0;
+}
