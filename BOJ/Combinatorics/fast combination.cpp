9047d3019258dccb6ac46c0058fc6be1d277fc68
Yunje Lee <yjyj102742@gmail.com>
2021-01-16 23:23:58 +0900

    Update fast combination.cpp

--- BOJ/Combinatorics/fast combination.cpp
+++ BOJ/Combinatorics/fast combination.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #define ll long long
-#define DIV 1000000007
+#define MOD 1000000007
 #define MAX 4000000
 
 using namespace std;
@@ -10,24 +10,24 @@ ll fact[MAX + 1], inv[MAX + 1];
 ll fastpow(ll a, ll pow) {
 	if (pow == 0) return 1LL;
 	ll ret = fastpow(a, pow / 2);
-	ret = (ret*ret) % DIV;
-	return pow % 2 == 1 ? (ret*a) % DIV : ret;
+	ret = (ret*ret) % MOD;
+	return pow % 2 == 1 ? (ret*a) % MOD : ret;
 }
 
 void init() {
 	fact[0] = 1;
 	fact[1] = 1;
-	for (ll i = 2; i <= MAX; i++) fact[i] = (fact[i - 1] * i) % DIV;
-	inv[MAX] = fastpow(fact[MAX], DIV - 2);
-	for (ll i = MAX - 1; i >= 0; i--) inv[i] = (inv[i + 1] * (i + 1)) % DIV;
+	for (ll i = 2; i <= MAX; i++) fact[i] = (fact[i - 1] * i) % MOD;
+	inv[MAX] = fastpow(fact[MAX], MOD - 2);
+	for (ll i = MAX - 1; i >= 0; i--) inv[i] = (inv[i + 1] * (i + 1)) % MOD;
 }
 
-ll comb(int n, int k) {
+ll nck(int n, int k) {
 	ll res = fact[n];
 	res *= inv[k];
-	res %= DIV;
+	res %= MOD;
 	res *= inv[n - k];
-	return res % DIV;
+	return res % MOD;
 }
 
 int main() {
